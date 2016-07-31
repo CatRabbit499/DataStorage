18adfb7b5667076dc71dcb26f5e57cf2466cf816
Nathan Meyer <Canon44444@gmail.com>
2016-07-30 20:04:09 -0400

    Worked on the layering of panels

--- src/dataStorage/DatabaseInterface.java
+++ src/dataStorage/DatabaseInterface.java
@@ -7,17 +7,12 @@ import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
 
 public class DatabaseInterface{
-//	public static final int HEIGHT = Integer.valueOf(new Long(Math.round(Toolkit.getDefaultToolkit().getScreenSize().getHeight())).intValue());
-//	public static final int WIDTH = Integer.valueOf(new Long(Math.round(Toolkit.getDefaultToolkit().getScreenSize().getWidth())).intValue());
 	public static final int HEIGHT = Integer.valueOf(new Long(Math.round(Toolkit.getDefaultToolkit().getScreenSize().getHeight())).intValue());
 	public static final int WIDTH = Integer.valueOf(new Long(Math.round(Toolkit.getDefaultToolkit().getScreenSize().getWidth())).intValue());
 	public static final ImageIcon BG_IMG = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("Cuttlefish.jpg"));
@@ -27,22 +22,20 @@ public class DatabaseInterface{
 	
 	public static void derp(){
 		
-		// Upper label panel
-		JLabel upperLabelPanelLabel = new JLabel();
-			if(isUHD) upperLabelPanelLabel.setFont(new Font("Arial", Font.PLAIN, 40));
-			upperLabelPanelLabel.setText("Label Text OMG");
-		JPanel upperLabelPanel = new JPanel();
-			upperLabelPanel.setSize(WIDTH/4, HEIGHT/4);
-			upperLabelPanel.add(upperLabelPanelLabel);
-			upperLabelPanel.setBackground(Color.GRAY);
-			
-		// Lower button panel
-		JButton lowerButtonPanelButton = new JButton();
-			lowerButtonPanelButton.setText("Click the button!");
-			if(isUHD) lowerButtonPanelButton.setFont(new Font("Arial", Font.PLAIN, 40));
-		JPanel lowerButtonPanel = new JPanel();
-			lowerButtonPanel.setSize(WIDTH/4, HEIGHT/4);
-			lowerButtonPanel.add(lowerButtonPanelButton);
+		// Layer 1 panel
+		JButton buttonOne = new JButton();
+			buttonOne.setText("Click the button!");
+			if(isUHD) buttonOne.setFont(new Font("Arial", Font.PLAIN, 40));
+		JLabel labelOne = new JLabel();
+			labelOne.setText("<html><font color='black'>Label Text OMG</font></html>");
+			if(isUHD) labelOne.setFont(new Font("Verdana", Font.PLAIN, 40));
+			labelOne.setBackground(Color.DARK_GRAY);
+			labelOne.setOpaque(true);
+		JPanel layerOnePanel = new JPanel();
+			layerOnePanel.setLayout(new BorderLayout());
+			layerOnePanel.setSize(BG_IMG_W, BG_IMG_H - (1000 * labelOne.getHeight()));
+			layerOnePanel.add(labelOne, BorderLayout.SOUTH);
+			layerOnePanel.setBackground(Color.GRAY);
 
 		// Background image
 		JLabel backgroundLabel = new JLabel();
@@ -53,27 +46,11 @@ public class DatabaseInterface{
 			backgroundPanel.add(backgroundLabel);
 			backgroundPanel.setSize(BG_IMG_W, BG_IMG_H);
 		
-		// Panel 1 test
-		JPanel panelTest = new JPanel();
-			panelTest.add(upperLabelPanel, BorderLayout.NORTH);
-			panelTest.add(lowerButtonPanel, BorderLayout.SOUTH);
-			panelTest.setSize(BG_IMG_W, BG_IMG_H);
-			panelTest.setOpaque(false);
-			
 		// Main content pane
 		JLayeredPane mainContentPane = new JLayeredPane();
 			mainContentPane.add(backgroundPanel, new Integer(1));
-			mainContentPane.add(panelTest, new Integer(2));
-			//mainContentPane.add(iFrame, new Integer(3));
+			mainContentPane.add(layerOnePanel, new Integer(2));
 		
-		// Internal frame test
-		JInternalFrame iFrame = new JInternalFrame();
-		JDesktopPane desktop = new JDesktopPane();
-			desktop.add(iFrame);
-		JPanel panel = new JPanel(new BorderLayout());
-			panel.setBorder(new TitledBorder("DesktopPane"));
-			panel.add(desktop);
-			
 		// Main frame
 		JFrame mainFrame = new JFrame("Cuttlefish Database Interface");
 			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
