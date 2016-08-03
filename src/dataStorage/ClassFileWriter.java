ab2561e34f5848458660d32a4ab1f3416098f00f
Nathan Meyer <Canon44444@gmail.com>
2016-08-03 16:11:26 -0400

    MASSIVE code cleanup

--- src/dataStorage/ClassFileWriter.java
+++ src/dataStorage/ClassFileWriter.java
@@ -1,3 +1,4 @@
+package dataStorage;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -8,63 +9,80 @@ import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
-import javax.swing.SwingConstants;
 
 public class ClassFileWriter{
-	public static File javaFile = new File("C:/users/canon/desktop/test.java");
-	public static File classFile = new File("C:/users/canon/desktop/test.class");
-	public static FileWriter f1;
-    public static void main(String[] args) throws IOException, InterruptedException{
+	public static final Font uniFont = new Font("Verdana", Font.PLAIN, 40);
+	public static JButton submitButton = new JButton("Submit");
+	public static JButton cancelButton = new JButton("Cancel");
+	public static JButton runButton = new JButton("Run");
+	public static JPanel buttonPanel = new JPanel();
+	public static JTextPane tp = new JTextPane();
+	public static File classFile;
+	public static File javaFile;
+	public static FileWriter outputFileWriter;
+	
+    public static void main(String[] args) throws IOException{
+		tp.setFont(new Font("Verdana", Font.PLAIN, 40));
+		setUpButtons();
+		createRunnableCode("C:/users/canon/desktop/test");
+    }
+    
+    
+
+	public static void createRunnableCode(String filename) throws IOException{
+    	classFile = new File("C:/users/canon/desktop/test.class");
+    	javaFile = new File("C:/users/canon/desktop/test.java");
     	if(javaFile.exists()) javaFile.delete();
     	if(classFile.exists()) classFile.delete();
-    	f1 = new FileWriter(javaFile);
-    	JLabel notifierLabel = new JLabel("", SwingConstants.CENTER);
-			notifierLabel.setFont(new Font("Verdana", Font.PLAIN, 40));
-			notifierLabel.setOpaque(false);
-		final JTextPane tp = new JTextPane();
-		tp.setFont(new Font("Verdana", Font.PLAIN, 40));
-		JButton submitButton = new JButton("Submit");
-			submitButton.setFont(new Font("Verdana", Font.PLAIN, 40));
-			submitButton.addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-					try{
-						TestBench.writeCode(tp.getText());
-						f1.close();
-						System.out.println(tp.getText());
-					}catch(Exception e1){
-						System.out.println(e1);
-					}
-				}
-			});
-		JButton cancelButton = new JButton("Cancel");
-			cancelButton.setFont(new Font("Verdana", Font.PLAIN, 40));
-			cancelButton.addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-					tp.setText("");
-				}
-			});
+    	outputFileWriter = new FileWriter(javaFile);
 			
-		JPanel buttonPanel = new JPanel();
-			buttonPanel.add(submitButton);
-			buttonPanel.add(cancelButton);
-		    		
 		JFrame frame = new JFrame("File Writer");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setSize(640, 480);
     		frame.setLocationRelativeTo(null);
     		frame.add(tp);
-    		notifierLabel.setText("Test Text");
-    		frame.add(notifierLabel);
     		frame.add(buttonPanel, BorderLayout.SOUTH);
     		frame.setVisible(true);
     }
+    
     public static void writeCode(String code) throws IOException{
-    	f1.write(code + "\n");
+    	outputFileWriter.write(code + "\n");
     }
     public static void writeCode() throws IOException{
-    	f1.write("\n");
+    	outputFileWriter.write("\n");
     }
+    private static void setUpButtons(){
+    	buttonPanel = new JPanel();
+		runButton = new JButton("Run");
+		submitButton = new JButton("Submit");
+		cancelButton = new JButton("Cancel");
+		submitButton.setFont(new Font("Verdana", Font.PLAIN, 40));
+		submitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				try{
+			    	writeCode(tp.getText());
+					outputFileWriter.close();
+					buttonPanel.removeAll();
+					buttonPanel.add(runButton);
+				}catch(IOException e1){
+					e1.printStackTrace();
+				}
+			}
+		});
+		cancelButton.setFont(new Font("Verdana", Font.PLAIN, 40));
+		cancelButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				tp.setText("");
+			}
+		});
+		buttonPanel.add(submitButton);
+		buttonPanel.add(cancelButton);
+		runButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				
+			}
+		});
+	}
 }
